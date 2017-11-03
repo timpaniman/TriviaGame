53147895832821741de48038259cb75f2c1100cf
unknown <timpaniman@gmail.com>
2017-11-03 09:07:32 -0700

    initial

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+< !DOCTYPE html >
+
+    <
+    html lang = "en-us" >
+    <
+    head >
+    <
+    meta charset = "UTF-8" >
+    <
+    title > Trivia < /title>
+
+<!-- Our CSS style sheet -->
+
+<
+link rel = "stylesheet"
+href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+integrity = "sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
+crossorigin = "anonymous" >
+
+    <!-- JQuery -->
+    <
+    script src = "https://code.jquery.com/jquery.js" > < /script>
+
+<
+script src = "assets/javascript/app.js" > < /script> <
+    link rel = "stylesheet"
+href = "assets/css/style.css" >
+
+    <
+    /head> <
+    body >
+    Hello <
+    script >
+    <
+    /script> <
+    /body> <
+    /html>
